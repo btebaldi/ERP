5dbafc0129725247775cdcb533ded04fff39f905
Teo <btebaldi@deskpaper.com>
2024-05-26 10:39:11 -0300

    Mes de Abril 2024

--- ERP calculation_v2.r
+++ ERP calculation_v2.r
@@ -40,23 +40,25 @@ log_message("PROCESSO INICIADO")
 
 
 base_ERP_full <- read_excel("Database/Import_base_ERP_economatica.xlsx", 
-                            range = cell_limits(ul = c(1,1), lr = c(NA, 13)),
+                            range = cell_limits(ul = c(1,1), lr = c(NA, 10)),
                             na = "-",
                             sheet = "Sheet1")
 
 
 
 # column  name regularization
-colnames(base_ERP_full) <- c("id", "Nome", "Classe", "Bolsa", "Tipo", 
-                             "Ativo", 
-                             "Ticker", "DPA", "LPA", "VPA", "Fechamento",
-                             "Setor", "Volume")
+colnames(base_ERP_full) <- c("id", "Nome", "Classe",
+                             "Ticker",
+                             "DPA", "LPA", "VPA",
+                             "Volume",
+                             "Setor",
+                             "Fechamento")
 
 base_ERP_full <- base_ERP_full %>% filter(!is.na(Ticker))
 
 # T10_Bond <- 4.35/100
-T10_Bond <- 4.25/100
-data_ref <- "2024-02-01"
+T10_Bond <- 4.69/100
+data_ref <- "2024-04-01"
 
 log_message(sprintf("TBond: %f", T10_Bond))
 log_message(sprintf("Lista de empresas consideradas: %s", paste(base_ERP_full$Ticker, collapse = ", ")))
